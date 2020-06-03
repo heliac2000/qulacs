9bf6e14cdbffe9a19398cfedfce55a28127dc56c
Takahashi Mamoru <pitman0016@gmail.com>
2020-06-03 18:33:17 +0900

    QASM フォーマットに変換した文字列を返す様に変更。 回路図を描画するのは呼び出し元で行う(sympy を利用)。

--- python/extra/circuit_convert_qasm_format.py
+++ python/extra/circuit_convert_qasm_format.py
@@ -1,26 +1,34 @@
 ##
 ## Convert QuantumCircuit to QASM format
 ##
+## Quantum Architectures: qasm2circ
+## https://www.media.mit.edu/quanta/qasm2circ/
+##
+## QASM is a simple text-format language for describing acyclic
+## quantum circuits composed from single qubit, multiply controlled
+## single-qubit gates, multiple-qubit, and multiple-qubit controlled
+## multiple-qubit gates.
+##
 
 from qulacs import QuantumCircuit, add_method, is_SWAP_gate
-from sympy import *
-from sympy.physics.quantum.qasm import Qasm
 
-def convert_to_qasm_format(self: QuantumCircuit) -> Qasm:
+def to_qasm(self: QuantumCircuit) -> str:
   lines = [f'qubit q_{i}' for i in range(self.get_qubit_count())]
   for gidx in range(self.get_gate_count()):
     gate = self.get_gate(gidx)
     name = gate.get_name().lower()
     control = gate.get_control_index_list()
     target = gate.get_target_index_list()
+    lc, lt = len(control), len(target)
     if is_SWAP_gate(gate):
       lines.append(f'{name} q_{target[0]}, q_{target[1]}')
-    elif len(control) == 0 :
+    elif lc == 0 and lt == 1:
       lines.append(f'{name} q_{target[0]}')
-    elif len(control) == 1 and len(target) == 1:
+    elif lc == 1 and lt == 1:
       lines.append(f'{name} q_{control[0]}, q_{target[0]}')
+    else:
+      pass # ToDo
 
-  self._qasm_source = '\n'.join(lines)
-  return Qasm(*lines)
+  return '\n'.join(lines)
 
-add_method(QuantumCircuit, convert_to_qasm_format)
+add_method(QuantumCircuit, to_qasm)
