33596c6644dfa34712177e2ebbb7224a7feb2185
Takahashi Mamoru <pitman0016@gmail.com>
2020-06-04 12:15:36 +0900

    Use importlib instead of the imp module(deprecated).

--- python/extra/__init__.py
+++ python/extra/__init__.py
@@ -5,11 +5,28 @@ except NameError:
   __QULACS_SETUP__ = False
 
 if not __QULACS_SETUP__:
-  from pkg_resources import get_distribution
-  from os import path
   from glob import glob
-  from imp import load_dynamic, load_source
+  from os import path
+  from sys import modules as sys_modules
+  from importlib._bootstrap import _exec
+  from importlib.machinery import SourceFileLoader
+  from importlib.util import spec_from_file_location, module_from_spec
+  from pkg_resources import get_distribution
 
+  # Load a shared library
+  def load_dynamic(module, path):
+    spec = spec_from_file_location(module, path)
+    mod = module_from_spec(spec)
+    sys_modules[module] = mod
+    spec.loader.exec_module(mod)
+
+  # Load a python code
+  def load_source(module, path):
+    loader = SourceFileLoader(module, path)
+    spec = spec_from_file_location(module, path, loader=loader)
+    _exec(spec, sys_modules[__name__])
+
+  # Load qulacs shared library
   load_dynamic(
     __name__,
     path.join(path.dirname(path.dirname(path.abspath(__file__))),
@@ -17,6 +34,7 @@ if not __QULACS_SETUP__:
                 get_distribution(__name__).egg_info, 'native_libs.txt')
               ).read().strip()))
 
+  # Load extra python codes
   for f in sorted(glob(path.join(path.dirname(path.abspath(__file__)), '*.py'))):
     if f != path.abspath(__file__): # don't load "__init__.py"
       load_source(__name__, f)
